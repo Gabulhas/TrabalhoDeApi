69cbfaeb98b96b4bb065b8d6dd8191eb4813cd6c
Tatiana <taticas.pimentel@gmail.com>
2018-01-20 19:43:00 +0000

    login

---
+++ TrabalhoDeApi_VB/Form2.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form2
+
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+End Class
