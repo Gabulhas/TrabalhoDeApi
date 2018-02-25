18f080e8d1fba00275fa1aee0412a9a5f55e675f
Guilherme Lopes <guilherme.bidarra@gmail.com>
2018-02-25 18:15:08 +0000

    Arranjado problema com picture box ;)

--- TrabalhoDeApi_VB/flashcards.vb
+++ TrabalhoDeApi_VB/flashcards.vb
@@ -5,27 +5,26 @@ Public Class flashcards
     Dim DbCon As New OleDb.OleDbConnection
     Dim dbUp As New OleDb.OleDbCommand
     Dim Read As OleDb.OleDbDataReader
-    Dim frente As String = Read("frente").ToString
-    Dim verso As String = Read("verso").ToString
+    
 
-    Private Sub ToolTip1_Popup(ByVal sender As System.Object, ByVal e As System.Windows.Forms.PopupEventArgs) Handles Ajuda.Popup
+    Private Sub ToolTip1_Popup(ByVal sender As System.Object, ByVal e As System.Windows.Forms.PopupEventArgs)
 
-        toolTip1.SetToolTip(PictureBox2, "Save changes");
+        ''toolTip1.SetToolTip(PictureBox2, "Save changes");
     End Sub
 
 
-    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.TextChanged
-        
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
         dbUp.Connection = DbCon
         DbCon.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\kokas\OneDrive\Documentos\GitHub\TrabalhoDeApi\TrabalhoDeApi_VB\Quiz.mdb"
         DbCon.Open()
         dbUp.CommandType = CommandType.Text
         dbUp.CommandText = "SELECT pergunta,resposta1,resposta2,resposta3 FROM Perguntas WHERE rnd(ID)"
         Read = dbUp.ExecuteReader
+        Dim frente As String = Read("frente").ToString
+        Dim verso As String = Read("verso").ToString
     End Sub
-
-
-    Private Sub PictureBox3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox3.Click
+    Private Sub PictureBox1_Click(sender As System.Object, e As System.EventArgs) Handles PictureBox1.Click
         Me.Hide()
         dashboardForm.Show()
 
